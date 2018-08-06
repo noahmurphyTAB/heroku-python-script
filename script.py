498076cf24d72b9ed047b001b86c08049f900235
Noah Murphy <noah.murphy@theappbusiness.com>
2018-08-06 16:25:13 +0100

    whaddup

--- script.py
+++ script.py
@@ -1,2 +1,9 @@
-while True:
-	print("I am working!")
+import requests
+
+url = "https://hooks.slack.com/services/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"
+with open('message.json') as file:
+    payload = file.read()
+
+headers = {"Content-type": "application/json"}
+
+r = requests.post(url, data = '{}'.format(payload), headers = headers)
